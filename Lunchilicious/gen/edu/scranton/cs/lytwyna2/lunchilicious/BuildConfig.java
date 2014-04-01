fc95b7dfdb0d9ccafb07e370a6d3f332557dd779
Andrew Lytwyn <lytwyna2@cs.scranton.edu>
2014-03-31 23:09:00 -0400

    first commit

---
+++ Lunchilicious/gen/edu/scranton/cs/lytwyna2/lunchilicious/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.scranton.cs.lytwyna2.lunchilicious;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
