d9a05806e4d69a23d26abe5c1057ae00951a5268
Andrew Lytwyn <lytwyna2@cs.scranton.edu>
2014-03-31 23:18:18 -0400

    added comment in MainActivity.java      line 15

--- Lunchilicious/src/edu/scranton/cs/lytwyna2/lunchilicious/MainActivity.java
+++ Lunchilicious/src/edu/scranton/cs/lytwyna2/lunchilicious/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	//Test change
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
